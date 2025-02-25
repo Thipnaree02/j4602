f5218d8e3b69f2681e4664cb396abe0f23915733
Thipnaree02 <65010914602@msu.ac.th>
2025-02-25 13:35:41 +0700

    a

--- connectdb.php
+++ connectdb.php
@@ -2,9 +2,9 @@
 $host = "localhost";
 $user = "root";
 $pwd = "";
-$db = "shop";
+$db = "shop4602";
 
 $conn = mysqli_connect ($host, $user, $pwd) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
-mysqli_select_db($conn, "shop")or die ("เลือกฐานข้อมูลไม่ได้");
+mysqli_select_db($conn, $db )or die ("เลือกฐานข้อมูลไม่ได้");
 mysqli_query($conn, "set names utf8")
 ?>
